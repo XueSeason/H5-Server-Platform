e5f8a039c92dc3bf6e4f570941ea038281630607
Season <jijie.xue@quvideo.com>
2016-10-28 10:40:21 +0800

    add resource branch

--- config/filepath.js
+++ config/filepath.js
@@ -4,19 +4,35 @@ const dist = path.resolve(__dirname, '../dist/')
 const resource = path.resolve(__dirname, '../resource/')
 
 const packDir = function (appId, version) {
-  return path.resolve(resource, appId, version)
+  return {
+    dev: path.resolve(resource, 'dev', appId, version),
+    pre: path.resolve(resource, 'pre', appId, version),
+    prod: path.resolve(resource, 'prod', appId, version)
+  }
 }
 
 const tarDir = function (appId, version) {
-  return path.resolve(resource, appId, version, `${appId}.tar`)
+  return {
+    dev: path.resolve(resource, 'dev', appId, version, `${appId}.tar`),
+    pre: path.resolve(resource, 'pre', appId, version, `${appId}.tar`),
+    prod: path.resolve(resource, 'prod', appId, version, `${appId}.tar`)
+  }
 }
 
 const certDir = function (appId, version) {
-  return path.resolve(resource, appId, version, 'CERT.json')
+  return {
+    dev: path.resolve(resource, 'dev', appId, version, 'CERT.json'),
+    pre: path.resolve(resource, 'pre', appId, version, 'CERT.json'),
+    prod: path.resolve(resource, 'prod', appId, version, 'CERT.json')
+  }
 }
 
 const manifestDir = function (appId, version) {
-  return path.resolve(resource, appId, version, 'Manifest.xml')
+  return {
+    dev: path.resolve(resource, 'dev', appId, version, 'Manifest.xml'),
+    pre: path.resolve(resource, 'pre', appId, version, 'Manifest.xml'),
+    prod: path.resolve(resource, 'prod', appId, version, 'Manifest.xml'),
+  }
 }
 
 const certPem = path.resolve(__dirname, '../rsa/cert.pem')
