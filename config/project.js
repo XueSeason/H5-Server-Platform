4dd055b7b00c89b575627ec4d1b781ff77022768
XueSeason <xueseason@icloud.com>
2016-11-02 11:23:52 +0800

    update config

--- config/project.js
+++ config/project.js
@@ -1,3 +1,18 @@
+// for appUrl
 const domain = "http://localhost:4000"
+// for fallback
+const cdnDomain = "https://xxxx.xxxxx.com"
+
+const product = 'xxxx'
+
+const oss = {
+  region: '',
+  accessKeyId: '',
+  accessKeySecret: '',
+  bucket: ''
+}
 
 exports.domain = domain
+exports.cdnDomain = cdnDomain
+exports.product = product
+exports.oss = oss
